a1e098a1fe75bc6f05274e610f96bad38b925b1e
Victor Calderon <calderon.j.victor@gmail.com>
2024-03-05 10:47:54 -0600

    hover animation applied, JS added

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+//     document.addEventListener("DOMContentLoaded", function () {
+//     // Get all dropdown toggles
+//     var dropdownToggles = document.querySelectorAll('[data-bs-hover="dropdown"]');
+//
+//     // Loop through each dropdown toggle
+//     dropdownToggles.forEach(function (toggle) {
+//     // Add mouseenter event listener
+//     toggle.addEventListener('mouseenter', function () {
+//     // Show the dropdown menu
+//     var dropdownMenu = toggle.nextElementSibling;
+//     dropdownMenu.classList.add('show');
+// });
+//
+//     // Add mouseleave event listener
+//     toggle.addEventListener('mouseleave', function () {
+//     // Hide the dropdown menu
+//     var dropdownMenu = toggle.nextElementSibling;
+//     dropdownMenu.classList.remove('show');
+// });
+// });
+// });
